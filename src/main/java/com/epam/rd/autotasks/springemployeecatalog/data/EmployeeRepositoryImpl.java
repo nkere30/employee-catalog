9df731c69adbf26067f444070488dfd784e90212
Kere <ninokereselidze30@gmail.com>
2024-05-10 16:00:53 +0400

    Commit N3

--- src/main/java/com/epam/rd/autotasks/springemployeecatalog/data/EmployeeRepositoryImpl.java
+++ src/main/java/com/epam/rd/autotasks/springemployeecatalog/data/EmployeeRepositoryImpl.java
@@ -16,10 +16,7 @@ import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class EmployeeRepositoryImpl implements EmployeeRepository{
@@ -34,7 +31,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
     @Override
     public Page<Employee> findAll(int page, int size, String sort) {
         String query = "SELECT * FROM EMPLOYEE ORDER BY " + sort;
-        List<Employee> employees = jdbcTemplate.query(query, new EmployeeRowMapper());
+        List<Employee> employees = jdbcTemplate.query(query, new EmployeeRowMapper(jdbcTemplate));
         return getPage(employees, page, size);
     }
 
@@ -42,13 +39,16 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
 
     @Override
     public Optional<Employee> findById(Long id, boolean fullChain) {
-        return Optional.empty();
+        String query = "SELECT * FROM EMPLOYEE WHERE ID = ?";
+        Employee employee = jdbcTemplate.queryForObject(query, new Object[]{id}, new EmployeeRowMapper(jdbcTemplate));
+        return Optional.ofNullable(employee);
     }
+
 
     @Override
     public Page<Employee> findEmployeesByManager(Long managerId, int page, int size, String sort) {
         String query = "SELECT * FROM EMPLOYEE WHERE MANAGER = ? ORDER BY " + sort;
-        List<Employee> employees = jdbcTemplate.query(query, new Object[]{managerId}, new EmployeeRowMapper());
+        List<Employee> employees = jdbcTemplate.query(query, new Object[]{managerId}, new EmployeeRowMapper(jdbcTemplate));
         return getPage(employees, page, size);
     }
 
@@ -56,7 +56,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
     @Override
     public Page<Employee> findEmployeesByDepartmentId(Long departmentId, int page, int size, String sort) {
         String query = "SELECT * FROM EMPLOYEE WHERE DEPARTMENT = ? ORDER BY " + sort;
-        List<Employee> employees = jdbcTemplate.query(query, new Object[]{departmentId}, new EmployeeRowMapper());
+        List<Employee> employees = jdbcTemplate.query(query, new Object[]{departmentId}, new EmployeeRowMapper(jdbcTemplate));
         return getPage(employees, page, size);
     }
     private Page<Employee> getPage(List<Employee> employees, int page, int size) {
@@ -65,12 +65,18 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
         List<Employee> employeeSubList = employees.subList(startIndex, endIndex);
         return new PageImpl<>(employeeSubList, PageRequest.of(page, size), employees.size());
     }
+
     private static class EmployeeRowMapper implements RowMapper<Employee> {
+        private final JdbcTemplate jdbcTemplate;
+
+        private EmployeeRowMapper(JdbcTemplate jdbcTemplate) {
+            this.jdbcTemplate = jdbcTemplate;
+        }
+
+        private static List<Long> employeeManager = new ArrayList<>();
 
         @Override
         public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
-            Map<Long, Employee> employees = new HashMap<>();
-            Map<Long, Long> employeeToManagerMap = new HashMap<>();
             Long id = resultSet.getLong("ID");
             String firstName = resultSet.getString("FIRSTNAME");
             String lastName = resultSet.getString("LASTNAME");
@@ -81,30 +87,38 @@ public class EmployeeRepositoryImpl implements EmployeeRepository{
             BigDecimal salary = BigDecimal.valueOf(resultSet.getDouble("SALARY"));
             Long managerId = resultSet.getLong("MANAGER");
             Employee manager = null;
-            Long departmentId = resultSet.getLong("DEPARTMENT");
-            Department department = (departmentId != 0) ? new Department(departmentId, null, null) : null;
-            if (managerId != 0) {
-                employeeToManagerMap.put(id, managerId);
+            if (employeeManager.isEmpty()) {
+                manager = findEmployee(managerId);
             }
-            Employee employee = new Employee(id, fullName, position, hired, salary, null, department);
-            employees.put(id, employee);
-            employeeToManagerMap.forEach((key, value) -> {
-                employees.put(key, findManager(key, employees, employeeToManagerMap));
-            });
+            Long departmentId = resultSet.getLong("DEPARTMENT");
+            Department department = findDepartment(departmentId);
+            Employee employee = new Employee(id, fullName, position, hired, salary, manager, department);
             return employee;
         }
 
-        private Employee findManager(Long id, Map<Long, Employee> employees, Map<Long, Long> employeeToManagerMap) {
-            Employee employee = employees.get(id);
-            Employee manager = employees.get(employeeToManagerMap.get(id));
-            Employee employeeWithManager;
-            if (manager != null) {
-                employeeWithManager = new Employee(employee.getId(), employee.getFullName(), employee.getPosition(), employee.getHired(), employee.getSalary(),
-                        findManager(manager.getId(), employees, employeeToManagerMap), employee.getDepartment());
-            } else {
-                employeeWithManager = employee;
-            }
-            return employeeWithManager;
+        private Department findDepartment(Long departmentId) {
+            if (departmentId == 0) return null;
+            String query = "SELECT * FROM DEPARTMENT WHERE ID = ?";
+            return jdbcTemplate.queryForObject(query, new Object[]{departmentId}, new DepartmentRowMapper());
         }
+
+        private Employee findEmployee(Long managerId) {
+            if (managerId == 0) return null;
+            employeeManager.add(managerId);
+            String query = "SELECT * FROM EMPLOYEE WHERE ID = ?";
+            return jdbcTemplate.queryForObject(query, new Object[]{managerId}, new EmployeeRowMapper(jdbcTemplate));
+        }
+
+        private static class DepartmentRowMapper implements RowMapper<Department> {
+
+            @Override
+            public Department mapRow(ResultSet resultSet, int i) throws SQLException {
+                Long id = resultSet.getLong("ID");
+                String name = resultSet.getString("NAME");
+                String location = resultSet.getString("LOCATION");
+                return new Department(id, name, location);
+            }
+        }
+
     }
 }
